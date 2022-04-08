e581b39c3739f3c89c9e7f57c70679e96b2a5d6b
mal1ing <ghfkd1235.com>
2022-04-08 18:16:17 +0900

    테스트

--- src/main/java/com/example/springsakura/GitTest.java
+++ src/main/java/com/example/springsakura/GitTest.java
@@ -2,7 +2,7 @@ package com.example.springsakura;
 
 public class GitTest {
     public static void main(String[] args){
-        System.out.println("GIT 연결 테스트입니다.");
+        System.out.println("GIT 연결 테스트입니다람쥐.");
     }
 
 }
